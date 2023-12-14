0c7c3c1ba53233945578c64ae00f886c56fefc55
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-14 19:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ console.log(tbody);
 let first_child= document.querySelectorAll("tr")[0];
 console.log(first_child);
 
-let cell_NO=0;
+// let cell_NO=0;
 function insert_Row() {
     //Write your code here
 	const tr= document.createElement("tr");
 	const td1= document.createElement("td");
 	const td2= document.createElement("td");
-	cell_NO =++cell_NO;
-	td1.innerText= `New Cell${cell_NO}`;
-	td2.innerText= `New Cell${cell_NO}`;
+	// cell_NO =++cell_NO;
+	td1.innerText= "New Cell1";
+	td2.innerText= "New Cell1";
 
 	tr.append(td1,td2);
 	tbody.insertBefore(tr, first_child);
