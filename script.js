740f6083372d8a94e3998f4947f47df7892ee596
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-14 19:15:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const table= document.getElementById("sampleTable");
+let tbody= document.querySelector("tbody");
+console.log(tbody);
+let first_child= document.querySelectorAll("tr")[0];
+console.log(first_child);
+
+let cell_NO=0;
 function insert_Row() {
     //Write your code here
+	const tr= document.createElement("tr");
+	const td1= document.createElement("td");
+	const td2= document.createElement("td");
+	cell_NO =++cell_NO;
+	td1.innerText= `New Cell${cell_NO}`;
+	td2.innerText= `New Cell${cell_NO}`;
+
+	tr.append(td1,td2);
+	tbody.insertBefore(tr, first_child);
+	first_child= tr;
+	
+	
   
   
 }
